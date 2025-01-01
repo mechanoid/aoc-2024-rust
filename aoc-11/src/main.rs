d216273229fe7d8b3efbb8eb7c98c5b555c87d7e
Falk Hoppe <falk.hoppe@innoq.com>
2025-01-01 16:55:48 +0100

    day-11: part 2 solved with counts only
    maintaining the full list of stones was impossible (but fun to try ;))

--- aoc-11/src/main.rs
+++ aoc-11/src/main.rs
@@ -1,9 +1,9 @@
 mod warping;
 use std::fs;
-use warping::{blink_n_times, parse};
+use warping::{parse, stone_count_for_n_blinks};
 
-#[tokio::main]
-async fn main() {
+// #[tokio::main]
+fn main() {
     let blink_stones = fs::read_to_string("./data/warp_stones.txt")
         .expect("Should have been able to read the file");
     let blink_stones = parse(&blink_stones);
@@ -14,9 +14,9 @@ async fn main() {
     //     blinked.len()
     // );
 
-    let blinked = blink_n_times(&blink_stones, 75).await;
-    println!(
-        "There are {} stones after 75 times blinking!",
-        blinked.len()
-    );
+    // let blinked = stone_count_for_n_blinks(&blink_stones, 25);
+    // println!("There are {} stones after 25 times blinking!", blinked);
+
+    let blinked = stone_count_for_n_blinks(&blink_stones, 75);
+    println!("There are {} stones after 75 times blinking!", blinked);
 }
--- aoc-11/src/warping.rs
+++ aoc-11/src/warping.rs
@@ -1,79 +1,43 @@
 use memoize::memoize;
-use std::time::Instant;
-use tokio::task::JoinSet;
 
-pub type Stone = u32;
+pub type StoneValue = u64;
+pub type StoneCount = u64;
 
-pub fn parse(blink_stones: &str) -> Vec<Stone> {
+pub fn parse(blink_stones: &str) -> Vec<StoneValue> {
     blink_stones
         .trim()
         .split(" ")
-        .map(|stone| stone.parse::<Stone>().unwrap())
-        .collect::<Vec<Stone>>()
+        .map(|stone| stone.parse::<StoneValue>().unwrap())
+        .collect::<Vec<StoneValue>>()
 }
 
 #[memoize]
-fn warp(stone: Stone) -> (Stone, Option<Stone>) {
-    match stone {
-        0 => (1, None),
-        i if i.to_string().len() % 2 == 0 => {
-            let i = i.to_string();
-            let (a, b) = i.split_at(i.len() / 2);
-            (
-                a.parse::<Stone>().unwrap(),
-                Some(b.parse::<Stone>().unwrap()),
-            )
-        }
-        _ => (stone * 2024, None),
+fn number_of_stones_after_blink(stone: StoneValue, mutations: u8) -> StoneCount {
+    if mutations == 0 {
+        return 1;
     }
+
+    if stone == 0 {
+        return number_of_stones_after_blink(1, mutations - 1);
+    } else if stone.to_string().len() % 2 == 0 {
+        let i = stone.to_string();
+        let (a, b) = i.split_at(i.len() / 2);
+        return number_of_stones_after_blink(a.parse::<StoneValue>().unwrap(), mutations - 1)
+            + number_of_stones_after_blink(b.parse::<StoneValue>().unwrap(), mutations - 1);
+    }
+
+    return number_of_stones_after_blink(stone * 2024, mutations - 1);
 }
 
-async fn blink(blink_stones: Vec<Stone>) -> Vec<Stone> {
-    let mut blinked = vec![];
+pub fn stone_count_for_n_blinks(blink_stones: &Vec<StoneValue>, n: u8) -> StoneCount {
+    let mut stone_count: StoneCount = 0;
 
     for stone in blink_stones {
-        let (update, new) = warp(stone);
-        blinked.push(update);
-        if let Some(new) = new {
-            blinked.push(new);
-        }
+        let stones_for_stone = number_of_stones_after_blink(*stone, n);
+        stone_count += stones_for_stone;
     }
 
-    return blinked;
-}
-
-async fn blink_chunked(blink_stones: Vec<Stone>) -> Vec<Stone> {
-    let mut set = JoinSet::new();
-
-    for chunk in blink_stones.chunks(10000) {
-        set.spawn(blink(chunk.to_vec()));
-    }
-
-    let result = set.join_all().await;
-    return result
-        .iter()
-        .flat_map(|el| el.clone())
-        .collect::<Vec<Stone>>();
-}
-
-pub async fn blink_n_times(blink_stones: &Vec<Stone>, n: u8) -> Vec<Stone> {
-    println!("\n\nstarting to blink!\n");
-    let mut blinked = blink_stones.clone();
-
-    for i in 0..n {
-        let now = Instant::now();
-        blinked = blink_chunked(blinked).await;
-        let elapsed_time = now.elapsed();
-        println!(
-            "Blinked for {} times: we have {} stones now! ({}ms)",
-            i + 1,
-            blinked.len(),
-            elapsed_time.as_millis()
-        );
-    }
-
-    println!("stop blinking!!!\n\n");
-    return blinked;
+    return stone_count;
 }
 
 // ###############################################################################################################
@@ -84,44 +48,16 @@ pub async fn blink_n_times(blink_stones: &Vec<Stone>, n: u8) -> Vec<Stone> {
 
 #[cfg(test)]
 mod tests {
-    use super::{blink, blink_n_times, Stone};
+    use super::{stone_count_for_n_blinks, StoneValue};
 
-    #[tokio::test]
-    async fn test_blink() {
-        let stones: Vec<Stone> = vec![0, 1, 10, 99, 999];
-        let result = blink(stones).await;
-        assert_eq!(result, vec![1, 2024, 1, 0, 9, 9, 2021976]);
-    }
+    #[test]
+    fn test_stone_count_for_n_blinks() {
+        let stones: Vec<StoneValue> = vec![125, 17];
+        let result = stone_count_for_n_blinks(&stones, 6);
+        assert_eq!(result, 22);
 
-    #[tokio::test]
-    async fn test_blink_n_times() {
-        let stones: Vec<Stone> = vec![125, 17];
-
-        let result = blink_n_times(&stones, 1).await;
-        assert_eq!(result, vec![253000, 1, 7]);
-
-        let result = blink_n_times(&stones, 2).await;
-        assert_eq!(result, vec![253, 0, 2024, 14168]);
-
-        let result = blink_n_times(&stones, 3).await;
-        assert_eq!(result, vec![512072, 1, 20, 24, 28676032]);
-
-        let result = blink_n_times(&stones, 4).await;
-        assert_eq!(result, vec![512, 72, 2024, 2, 0, 2, 4, 2867, 6032]);
-
-        let result = blink_n_times(&stones, 5).await;
-        assert_eq!(
-            result,
-            vec![1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32]
-        );
-
-        let result = blink_n_times(&stones, 6).await;
-        assert_eq!(
-            result,
-            vec![
-                2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6,
-                0, 3, 2
-            ]
-        );
+        let stones: Vec<StoneValue> = vec![125, 17];
+        let result = stone_count_for_n_blinks(&stones, 25);
+        assert_eq!(result, 55312);
     }
 }
